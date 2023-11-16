33940fa19a19c59bc111f79cdf003490cbfdf583
Jacob Gormly <gormlyja@mail.uc.edu>
2023-11-16 13:25:36 -0500

    Replace scan file in main

--- src/main/scan.py
+++ src/main/scan.py
@@ -1,15 +1,37 @@
 import subprocess
+import requests
 
-# define the command to list installed programs using wmic
+# Function to search for CVEs for a given application and version
+def search_cve(application_name, version):
+    base_url = "https://services.nvd.nist.gov/rest/json/cves/1.0"
+    cpe = f"cpe:2.3:a:{application_name}:{version}"
+
+    params = {
+        'resultsPerPage': '10',  # adjust as needed
+        'cpeMatchString': cpe
+    }
+
+    response = requests.get(base_url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return f"Error: {response.status_code}"
+
+# Define the command to list installed programs using wmic
 scan = 'wmic product get name,version'
 
-output_file = 'programs.txt'
-
-# run the command and save the output to a text file
-with open(output_file, 'w') as file:
-    result = subprocess.run(scan, stdout=file, stderr=subprocess.PIPE, text=True, shell=True)
+# Run the command
+result = subprocess.run(scan, capture_output=True, text=True, shell=True)
 
 if result.returncode == 0:
-    print(f"Installed programs list saved to {output_file}")
+    output = result.stdout
+    # Process each line of the output
+    for line in output.splitlines():
+        parts = line.strip().split()
+        if len(parts) >= 2:
+            app_name = parts[0]
+            app_version = parts[1]
+            cve_results = search_cve(app_name, app_version)
+            print(f"CVEs for {app_name} {app_version}: {cve_results}")
 else:
     print("Error:", result.stderr)
