75c420253282bfb05d244134b6a280c641757de9
Noah <noahamann@gmail.com>
2024-01-24 14:28:52 -0500

    Create rss.py

---
+++ src/main/rss.py
@@ -0,0 +1,19 @@
+import feedparser
+
+# List of RSS feed URLs
+rss_urls = [
+    'https://www.cisa.gov/cybersecurity-advisories/all.xml',
+    'https://www.us-cert.gov/ncas/current-activity.xml'
+   
+]
+
+# Iterate through the list of feed URLs
+for rss_url in rss_urls:
+    feed = feedparser.parse(rss_url)
+    
+    print("RSS Feed:", rss_url)
+    
+    for entry in feed.entries:
+        print("Title:", entry.title)
+        print("Link:", entry.link)
+        print()
