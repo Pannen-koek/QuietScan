a4d59bb718c1d7d5bfbabb461c0abc68ba2a71b6
Jacob Gormly <gormlyja@mail.uc.edu>
2024-02-06 18:14:01 -0500

    Added panel switching functionality and cleaned up scan panel with new scan checklist

--- src/main/main.py
+++ src/main/main.py
@@ -1,12 +1,15 @@
-from os import system
-import subprocess
+import customtkinter
 import ttkbootstrap as tb
 import tkinter as tk
-from tkinter import ttk
-from ttkbootstrap.constants import *
 
 from about import about_text
 from src.main.scan import system_scan
+
+
+def raise_frame(focusedFrame, button):
+    # TODO Regenerate nav buttons with selected button different color if time allows in dev
+    # customtkinter.CTkButton.configure(button, fg_color='black')
+    focusedFrame.tkraise()
 
 
 def scan_button_use(textbox):
@@ -15,12 +18,14 @@ def scan_button_use(textbox):
     system_scan(textbox)
     textbox.config(state=tb.DISABLED)
 
+
 def show_about_dialog():
     about_dialog = tk.Toplevel(root)
     about_dialog.title("About")
     about_dialog.geometry("1200x720")
     about_label = tk.Label(about_dialog, text=about_text, wraplength=500, justify="left", font=("Helvetica", 18))
     about_label.pack()
+
 
 def button_fill():  # TODO implement screen change functionality as screens are made
     print("Navigation Occurring")
@@ -29,6 +34,8 @@ def button_fill():  # TODO implement screen change functionality as screens are 
 root = tk.Tk()
 root.title("QuietScan")
 root.geometry('1280x720')
+
+f1 = tk.Frame(root)
 
 # header frame
 headerFrame = tb.Frame(root, width=1200, height=150)
@@ -39,29 +46,46 @@ label.pack(side=tb.LEFT)
 
 # nav
 navFrame = tb.Frame(root, width=700, height=50)
-navFrame.place(x=10, y=100)
-
-sep = tb.Separator  # TODO implement this seperator between buttons
-
-homeButton = tb.Button(navFrame, class_="homeButton", text="Home", command=lambda: button_fill(), width=20)
+navFrame.place(x=20, y=75)
+sep = tb.Separator(navFrame, orient="vertical")  # TODO implement this seperator between buttons
+sep2 = tb.Separator(navFrame, orient="vertical")
+homeButton = customtkinter.CTkButton(navFrame, text="Scan", command=lambda: raise_frame(scanFrame, homeButton), width=20)
 homeButton.pack(side=tb.LEFT)
-
-aboutButton = ttk.Button(navFrame, text="About", command=show_about_dialog, width=20)
-aboutButton.pack(side=tk.LEFT)
-
-historyButton = tb.Button(navFrame, class_="historyButton", text="Scan History", command=lambda: button_fill(),
-                          width=20)
+sep.pack(side=tb.LEFT, padx=20)
+aboutButton = customtkinter.CTkButton(navFrame, text="About", command=lambda: raise_frame(aboutFrame, aboutButton), width=20)
+aboutButton.pack(side=tb.LEFT)
+sep2.pack(side=tb.LEFT, padx=20)
+historyButton = customtkinter.CTkButton(navFrame, text="Scan History", command=lambda: raise_frame(historyFrame, historyButton), width=20)
 historyButton.pack(side=tb.LEFT)
 
-# body frame - initiate scan and scan checklist boxes from wireframe  # TODO implement and position these boxes
-bodyFrame = tb.Frame(root, width=300, height=150)
-bodyFrame.place(x=10, y=150)
+scanFrame = tb.Frame(root, width=1400, height=500)
+aboutFrame = tb.Frame(root, width=1400, height=500)
+historyFrame = tb.Frame(root, width=1400, height=500)
 
-scanButton = tb.Button(bodyFrame, text="Start a new System Scan", command=lambda: scan_button_use(scan_output_box),
-                       width=50)
-scanButton.pack()
+frames = ( scanFrame, aboutFrame, historyFrame )
 
-scan_output_box = tb.ScrolledText(bodyFrame, pady=5, padx=5, height=30, width=150, state=tb.DISABLED)
-scan_output_box.pack()
+for frame in frames:
+    frame.place(x=20, y=150)
+
+
+# scan frame - initiate scan and scan checklist boxes from wireframe
+scanButton = customtkinter.CTkButton(scanFrame, text="Start a new System Scan",
+                                     command=lambda: scan_button_use(scan_output_box), width=50)
+scanButton.grid(row=0, column=0, columnspan=2, sticky=tb.W+tb.E)
+
+scan_output_box = tb.ScrolledText(scanFrame, height=30, width=150, state=tb.DISABLED)
+scan_output_box.grid(row=1, column=0, columnspan=2, sticky=tb.W+tb.E)
+
+space = tb.Frame(scanFrame, width=20)
+space.grid(row=1, column=2)
+
+scan_checklist = tb.LabelFrame(scanFrame, height=450, text="Scan Checklist")
+scan_checklist.grid(row=1, column=3, sticky=tb.W+tb.E+tb.N+tb.S)
+
+scan_step1 = tb.Checkbutton(scan_checklist, padding=10, width=40)
+scan_step1.grid(row=0)
+
+# start in home panel
+raise_frame(scanFrame, homeButton)
 
 root.mainloop()
