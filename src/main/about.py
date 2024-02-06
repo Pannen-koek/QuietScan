72990e207965ad43bede734b259365aa740c2ab9
ChristopherLiu6 <145885752+ChristopherLiu6@users.noreply.github.com>
2024-02-06 14:36:42 -0500

    Create about.py

---
+++ src/main/about.py
@@ -0,0 +1,10 @@
+about_text = """\
+QuietScan is a Free and Open-Source Vulnerability Scanner designed for both home and small business use cases. 
+
+Key Features:
+- Download and execute the application on a local host running Windows or Linux.
+- Query NIST CVE Database for vulnerability information.
+- Perform basic scanning functionality to identify vulnerabilities.
+- Provide remedies for identified vulnerabilities, including automatic remediation or links to resources for further information.
+- Offer a cost-free and open-source security scanning tool to users without access to sophisticated scanning software, ensuring systems adhere to recommended security practices and reducing the risk of cyberattacks.
+"""
