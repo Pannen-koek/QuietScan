63faf3049c4158d7b8c5fba94e1e08e703995217
Pannen-koek <guy.cooper.2020@gmail.com>
2024-02-08 17:04:38 -0500

    Work on REST API

--- src/feature/api/testapi.py
+++ src/feature/api/testapi.py
@@ -1,13 +1,30 @@
-from ast import parse
 import requests as re
 import os 
-import csv
+import json 
 
 api_key = os.getenv("API_Key_QS")
-api_url = "https://services.nvd.nist.gov/rest/json/cves/2.0/?keywordSearch=Java%208"
+api_url = "https://services.nvd.nist.gov/rest/json/cves/2.0/?keywordSearch=Discord"
 response = re.get(api_url)
 
-print (response.json())
-#parseresponse = response.json()
+parseresponse = response.json()
+pretty_response = json.dumps(parseresponse, indent=4)
+
+#print(pretty_response)
+
+
 #for item in parseresponse:
-#    print(parsepresonse['Total Results'])
+    #for item in item[4]:
+        #print(item['cve']['CVE_data_meta']['ID'])
+    #print(item['publishedDate'])
+    #print(item['lastModifiedDate'])
+    #print(item['summary'])
+    
+for item in parseresponse:
+    if isinstance(item, list) or isinstance(item, dict):
+        for sub_item in item[6]:
+            print(sub_item['cve']['id'])
+        print(item['publishedDate'])
+        print(item['lastModifiedDate'])
+        print(item['summary'])
+    else:
+        print("Invalid item type:", type(item))
