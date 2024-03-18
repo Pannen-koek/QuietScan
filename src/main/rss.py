a6e8e8c8952d5b922e40c7d74df754e7dab3b160
Jacob Gormly <gormlyja@mail.uc.edu>
2024-03-18 18:20:03 -0400

    Fixed return of RSS feeds

--- src/main/rss.py
+++ src/main/rss.py
@@ -1,10 +1,12 @@
 import feedparser
 import html
+import customtkinter
+import ttkbootstrap as tb
 
 # List of RSS feed URLs
 rss_urls = [
     'https://www.cisa.gov/cybersecurity-advisories/all.xml',
-    'https://www.us-cert.gov/ncas/current-activity.xml'
+    'https://www.us-cert.gov/ncas/current-activity.xml',
     'https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss-analyzed.xml',
     'https://krebsonsecurity.com/feed/',
     'https://feeds.feedburner.com/TheHackersNews?format=xml',
@@ -20,8 +22,6 @@ for rss_url in rss_urls:
     for entry in feed.entries:
         title = html.unescape(entry.title)
         link = html.unescape(entry.link)
-
-    for entry in feed.entries:
         print("Title:", html.escape(title))
-        print("Link:", link)
-        print()
+        print("Link:", link, "\n")
+
